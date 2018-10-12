b4a4ab69739d8555732baea812c11e7d6a6d5724
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-10-12 12:55:16 +0200

    prepare for deployment

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/api/posts", posts);
 // Server static assets if in production
 
 if (process.env.NODE_ENV === "production") {
-  // set static folder
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
